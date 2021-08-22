e7d2eb8de0d153ca1f5d8d0c42898934056cab99
hansmaad <hansmaad@gmail.com>
2021-08-22 12:50:35 +0200

    Toggle cams.

--- js/home/index.js
+++ js/home/index.js
@@ -1,10 +1,8 @@
 import { query, html, on } from '../common/index.js';
 
 export function home(ctx) {
-    ctx.container.innerHTML = '<h1>Cam</h1>';
-
+    ctx.container.innerHTML = '';
     const destroy = [cam(ctx.container)];
-
     return (ctx) => {
         destroy.forEach(d => d());
     }
@@ -12,15 +10,28 @@ export function home(ctx) {
 
 
 const cam = (container) => {
+    const camContainer = html(`<div class="app-cam"></div>`);
+    container.appendChild(camContainer);
     const player = html(`<video autoplay></video>`);
     const off = [];
+    let currentStream;
 
+    const stopCam = () => {
+        if (currentStream) {
+            currentStream.getTracks().forEach(track => {
+                track.stop();
+            });
+            currentStream = null;
+        }
+    }
     const activateCam = (id) => {
+        stopCam();
         const videoConstraints = { deviceId: id ? { exact: id }: undefined };
         return navigator.mediaDevices.getUserMedia({
             audio: false,
             video: videoConstraints
         }).then(stream => {
+            currentStream = stream;
             player.srcObject = stream;
         });
     };
@@ -28,7 +39,7 @@ const cam = (container) => {
     const selectBtn = (camera, index) => {
         const btn = html(`<button class="btn">${camera.label || 'Camera ' + index}</button>`)
         const destroy = on(btn, 'click', () => {
-            activateCam(camera);
+            activateCam(camera.deviceId);
         });
         off.push(destroy);
         return btn;
@@ -39,14 +50,15 @@ const cam = (container) => {
         cameras.forEach((c, index) => {
             selectContainer.appendChild(selectBtn(c, index));
         })
-        container.appendChild(selectContainer);
+        camContainer.appendChild(selectContainer);
     }
 
-    container.appendChild(player);
-
+    camContainer.innerHTML = 'Loading...';
     navigator.mediaDevices.enumerateDevices().then(mediaDevices => {
+        camContainer.replaceChildren(player);
+
         const cameras = mediaDevices.filter(dev => dev.kind === 'videoinput');
-        if (cameras.length === 3) {
+        if (cameras.length < 0) {
             activateCam();
         }
         else {
